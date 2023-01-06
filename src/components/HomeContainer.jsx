c03a4d17fea1d42b2f38d5b95ee93f99346bec20
Crownkay <adekomsk@gmail.com>
2023-01-06 09:40:49 -0700

    commit

---
+++ src/components/HomeContainer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Bike from "../img/delivery.png";
+
+const HomeContainer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-[calc(100%-88px)]">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-4 md:gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-200 md:px-3 px-2 py-1 rounded-tr-full rounded-br-full">
+          <p className="text-base text-orange-600 font-semi-bold">
+            Bike Delivery
+          </p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img
+              src={Bike}
+              className="w-full h-full object-contain"
+              alt="deliveryBike"
+            />
+          </div>
+        </div>
+
+        <p className="text-[2.5rem] lg:text-[4rem] font-bold tracking-wide text-headingColor text-left">
+          The Fastest Delivery in{" "}
+          <span className="text-orange-600 text-[3rem] lg:text-[4.5rem]">
+            Your City
+          </span>
+        </p>
+        <p className="text-base text-textColor text-left md:w-[80%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum velit
+          fugit quis voluptatem nostrum dolores natus, temporibus, non magni,
+          molestias eius vitae praesentium minima illum eligendi cumque. Vel,
+          nemo recusandae!
+        </p>
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
+    </div>
+  );
+};
+
+export default HomeContainer;
